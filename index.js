009c84b5daf599ab158b352272edb6825fc56b83
rajaraja123 <95062887+rajaraja123@users.noreply.github.com>
2023-05-31 18:24:14 +0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import express from "express";
-import cors from "cors";
-
+import express from 'express';
+import cors from 'cors';
+import MahasiswaRoute from './routes/mahasiswaroutes.js';
 const app = express();
+const PORT = 8080;
+
 app.use(cors());
 app.use(express.json());
-app.listen(5000, ()=> console.log('Server up'))
+app.use(MahasiswaRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
